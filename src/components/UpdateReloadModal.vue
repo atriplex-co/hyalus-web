e5c482acf6224dc1a2a6ffd9b3de78c6988ac53d
Asineth <asineth@pm.me>
2024-01-12 17:05:03 -0500

    force restart desktop app for update

--- src/components/UpdateReloadModal.vue
+++ src/components/UpdateReloadModal.vue
@@ -1,21 +1,31 @@
 <template>
-  <ModalBase title="Reload to Update" submit-text="Reload" @close="$emit('close')" @submit="submit">
+  <ModalBase
+    :title="!isDesktop ? 'Reload to Update' : 'Restart to Update'"
+    submit-text="Reload"
+    @close="$emit('close')"
+    @submit="submit"
+  >
     <template #icon>
       <RefreshIcon />
     </template>
     <template #main>
-      <p class="mb-1">Reload the app for the latest update?</p>
+      <p class="mb-1">{{ !isDesktop ? "Reload" : "Restart" }} the app for the latest update?</p>
     </template>
   </ModalBase>
 </template>
 
 <script lang="ts" setup>
+import { isDesktop } from "@/global/helpers";
 import ModalBase from "./ModalBase.vue";
 import RefreshIcon from "@/icons/RefreshIcon.vue";
 
 defineEmits(["close"]);
 
 const submit = () => {
-  location.reload();
+  if (isDesktop) {
+    window.HyalusDesktop!.restart();
+  } else {
+    location.reload();
+  }
 };
 </script>
